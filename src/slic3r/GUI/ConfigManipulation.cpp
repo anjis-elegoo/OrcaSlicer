ef447e4ef2967bd38de6160a34d6e78638031077
wujie <jie.wu@elegoo.com>
2026-09-10 19:03:15 +0800

    fix: preserve logical filament counts and enforce exclusive multimaterial modes

--- src/slic3r/GUI/ConfigManipulation.cpp
+++ src/slic3r/GUI/ConfigManipulation.cpp
@@ -1018,10 +1018,10 @@ void ConfigManipulation::toggle_print_fff_options(DynamicPrintConfig *config, in
     //     toggle_field(el, have_sequential_printing);
     toggle_field("print_order", !have_sequential_printing);
 
-    toggle_field("single_extruder_multi_material", !is_BBL_Printer);
-    toggle_field("multi_extruder_multi_filament", !is_BBL_Printer);
-
     auto bSEMM = preset_bundle->printers.get_edited_preset().config.opt_bool("single_extruder_multi_material");
+    toggle_field("single_extruder_multi_material", !is_BBL_Printer &&
+                 !preset_bundle->printers.get_edited_preset().config.opt_bool("multi_extruder_multi_filament"));
+    toggle_field("multi_extruder_multi_filament", !is_BBL_Printer && !bSEMM);
     const bool supports_wipe_tower_2 = !is_BBL_Printer && preset_bundle->printers.get_edited_preset().config.opt_enum<WipeTowerType>("wipe_tower_type") == WipeTowerType::Type2;
 
     toggle_field("ooze_prevention", !bSEMM);
